e4aed91ff37c41d91cd367eacb029d283544d721
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-03-05 14:00:10 +0100

    u

---
+++ find_py.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+if len(sys.argv) >= 2:
+    mot_cle = " ".join(sys.argv[1:])
+else:
+    mot_cle = input("Mot à chercher ? : ")
+
+def dirl(path):
+    files = []
+    listdir = os.listdir(path)
+    for item in listdir:
+        if os.path.isdir(item):
+            dirl(path + "/" + item)
+        elif item.endswith(".py"):
+            files.append(path + "/" + item)
+    return files
+
+py_mot = []
+files = dirl(".")
+
+print("%d fichiers python ont été trouvés dans le répertoire courant" % len(files))
+# print(len(files))
+for file in files:
+    # print(file)
+    # print("Opening %s" % file)
+    fo = open(file, "r", encoding = "utf8")
+    for line in fo.readlines():
+        if mot_cle in line:
+            py_mot.append(file)
+            break
+
+if len(py_mot) == 0:
+    print("Aucun fichier .py contient \"%s\"" % mot_cle)
+else:
+    print("Les fichiers suivants contiennent \" %s\" (%d)" % (mot_cle, len(py_mot)))
+    print("%s : " % [file for file in py_mot])
+    req = input("Quoi ouvrir ? : ")
+    print([req])
+    if req.isnumeric():
+        for i,file in enumerate(py_mot):
+            if i == int(req):
+                break
+            os.system("atom " + file)
+    for file in py_mot:
+        if req in file:
+            print("Ouverture de", file)
+            os.system("atom \"%s\"" % file)
